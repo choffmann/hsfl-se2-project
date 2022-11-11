e68c69a14944118bc1fce075a73e5b7768d73596
Dimitri Dorn <dimitri.dorn2@stud.hs-flensburg.de>
2022-11-11 13:00:59 +0100

    Projekt angelegt

---
+++ src/main/kotlin/com/hsfl/springbreak/spring_break/SpringBreakApplication.kt
@@ -0,0 +1,11 @@
+package com.hsfl.springbreak.spring_break
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBreakApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBreakApplication>(*args)
+}
---
+++ src/test/kotlin/com/hsfl/springbreak/spring_break/SpringBreakApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hsfl.springbreak.spring_break
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBreakApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
