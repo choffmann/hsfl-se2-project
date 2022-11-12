1795265d99279e72749d926facddc11cb9b7e0b6
Cedrik Hoffmann <hoffmann@geograt.de>
2022-11-12 17:09:43 +0100

    Implement ApplicationRunner to initialise database

--- src/main/kotlin/com/hsfl/springbreak/spring_break/demo/DemoComponent.kt
+++ src/main/kotlin/com/hsfl/springbreak/spring_break/demo/DemoComponent.kt
@@ -1,15 +1,18 @@
 package com.hsfl.springbreak.spring_break.demo
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 
 @Component
-class DemoComponent(val repository: DemoDataRepository) {
+class DemoComponent(val repository: DemoDataRepository): ApplicationRunner {
     // Save DemoDate in database on init
-    init {
+    override fun run(args: ApplicationArguments?) {
         listOf(
             DemoData(firstName = "Hans", lastName = "Müller"),
             DemoData(firstName = "Dimi", lastName = "Dorni"),
             DemoData(firstName = "Hektor", lastName = "Panzer"),
+            DemoData(firstName = "Arnold", lastName = "Schwarzenegger"),
         ).forEach { data ->
             repository.save(data)
         }
