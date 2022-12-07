e9a1822e2955536bbc6c68502672802c80ffc182
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2022-12-07 11:09:06 +0100

    Make router running

--- frontend/src/main/kotlin/com/hsfl/springbreak/frontend/App.kt
+++ frontend/src/main/kotlin/com/hsfl/springbreak/frontend/App.kt
@@ -5,15 +5,21 @@ import com.hsfl.springbreak.frontend.client.presentation.state.AuthState
 import com.hsfl.springbreak.frontend.client.presentation.state.UiEventState
 import com.hsfl.springbreak.frontend.components.Header
 import com.hsfl.springbreak.frontend.components.routes.Home
+import com.hsfl.springbreak.frontend.components.routes.RecipeFormular
 import com.hsfl.springbreak.frontend.components.snackbar.MessageSnackbar
 import com.hsfl.springbreak.frontend.context.AuthorizedContext
 import com.hsfl.springbreak.frontend.context.UiStateContext
 import com.hsfl.springbreak.frontend.di.di
 import com.hsfl.springbreak.frontend.utils.collectAsState
 import mui.material.CssBaseline
+import mui.material.Typography
 import org.kodein.di.instance
 import react.*
 import react.dom.client.createRoot
+import react.router.Route
+import react.router.Routes
+import react.router.dom.BrowserRouter
+import react.router.dom.HashRouter
 
 fun main() {
     createRoot(document.createElement("div").also { document.body.appendChild(it) }).render(Root.create())
@@ -31,15 +37,31 @@ private val Root = FC<Props> {
     }
 }
 
-private val App = FC<Props> {props ->
+private val App = FC<Props> { props ->
     // Default Css
     CssBaseline()
 
     MessageSnackbar()
 
     // Display Header
-    Header {
-        onLogoClicked = { println("click") }
-        Home()
+    BrowserRouter {
+        Header {
+            onLogoClicked = { println("click") }
+            Routes {
+                Route {
+                    index = true
+                    element = Home.create()
+                }
+                Route {
+                    index = false
+                    path = "create"
+                    element = RecipeFormular.create()
+                }
+                Route {
+                    path = "*"
+                    element = Typography.create { +"404 not found" }
+                }
+            }
+        }
     }
 }
---
+++ frontend/src/main/kotlin/com/hsfl/springbreak/frontend/components/routes/RecipeFormular.kt
@@ -0,0 +1,8 @@
+package com.hsfl.springbreak.frontend.components.routes
+
+import react.FC
+import react.Props
+
+val RecipeFormular = FC<Props> {
+
+}
