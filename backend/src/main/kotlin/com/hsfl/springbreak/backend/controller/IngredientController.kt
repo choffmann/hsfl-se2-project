c924350a6d3066d3ca53d77e302488accdd9ab64
dido3296 <dimitri.dorn2@stud.hs-flensburg.de>
2022-12-07 13:28:06 +0100

    Ingredient Controller angepasst

--- backend/src/main/kotlin/com/hsfl/springbreak/backend/controller/IngredientController.kt
+++ backend/src/main/kotlin/com/hsfl/springbreak/backend/controller/IngredientController.kt
@@ -1,13 +1,14 @@
 package com.hsfl.springbreak.backend.controller
 
+import com.hsfl.springbreak.backend.entity.IngredientEntity
 import com.hsfl.springbreak.backend.entity.IngredientRecipeEntity
-import com.hsfl.springbreak.backend.entity.RecipeEntity
 import com.hsfl.springbreak.backend.model.ApiResponse
+import com.hsfl.springbreak.backend.model.Ingredient
 import com.hsfl.springbreak.backend.model.IngredientRecipe
-import com.hsfl.springbreak.backend.model.Recipe
 import com.hsfl.springbreak.backend.repository.IngredientRecipeRepository
-import com.hsfl.springbreak.backend.repository.RecipeRepository
+import com.hsfl.springbreak.backend.repository.IngredientRepository
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -15,11 +16,22 @@ import org.springframework.web.bind.annotation.RestController
 
 @CrossOrigin("http://localhost:3000")
 @RestController
-class IngredientController(val ingredientRecipeRepository: IngredientRecipeRepository) {
+class IngredientController(val ingredientRecipeRepository: IngredientRecipeRepository, val ingredientRepository: IngredientRepository) {
 
-    @PostMapping("api/ingredientRecepie")
-    fun createNewingredientRecipe(@RequestBody newingredientRecipe: IngredientRecipe): ApiResponse<IngredientRecipe> {
-        val savedIngredientRecipe = ingredientRecipeRepository.save(IngredientRecipeEntity.fromDto(newingredientRecipe)).toDto()
+    @GetMapping("api/ingredients")
+    fun getAllIngredients(): ApiResponse<List<Ingredient>> {
+        val ingredientEntities = ingredientRepository.findAll().distinct()
+        return if (ingredientEntities.isNotEmpty()) {
+            val ingredients = ingredientEntities.map { it.toDto() }
+            ApiResponse(data = ingredients, success = true)
+        } else {
+            ApiResponse(error = "No ingredients available", success = false)
+        }
+    }
+
+    @PostMapping("api/ingredientRecipe")
+    fun createNewIngredientRecipe(@RequestBody newIngredientRecipe: IngredientRecipe): ApiResponse<IngredientRecipe> {
+        val savedIngredientRecipe = ingredientRecipeRepository.save(IngredientRecipeEntity.fromDto(newIngredientRecipe)).toDto()
         print("-------- "+savedIngredientRecipe+"----- \n")
         return if (savedIngredientRecipe != null) {
             ApiResponse(data = savedIngredientRecipe, success = true)
