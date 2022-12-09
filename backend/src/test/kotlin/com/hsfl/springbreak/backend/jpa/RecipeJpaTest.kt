4a275e2aaf233fe7f0760a1ffec28b63c9c081c4
dido3296 <dimitri.dorn2@stud.hs-flensburg.de>
2022-12-09 20:13:48 +0100

    Changes by Maxim

--- backend/src/test/kotlin/com/hsfl/springbreak/backend/jpa/RecipeJpaTest.kt
+++ backend/src/test/kotlin/com/hsfl/springbreak/backend/jpa/RecipeJpaTest.kt
@@ -2,7 +2,10 @@ package com.hsfl.springbreak.backend.jpa
 
 import com.hsfl.springbreak.backend.controller.RecipeController
 import com.hsfl.springbreak.backend.entity.*
+import com.hsfl.springbreak.backend.model.IngredientRecipe
+import com.hsfl.springbreak.backend.model.Recipe
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -18,82 +21,74 @@ class RecipeJpaTest {
     @Autowired
     private lateinit var controller: RecipeController
     private var id: Long = 0
-    private var testRecipe = RecipeEntity(
-        id = null, //TODO: Id wird nicht automatisch vergeben
+    private var testRecipe = Recipe.CreateRecipe(
         title = "Schockomuffins",
         shortDescription = "Diese Muffins lieben alle! So schnell und einfach vorzubereiten und unglaublich saftig – das ist unser beliebtes Schokomuffin-Rezept.",
         price = 5.00,
         duration = 10.00,
-        category = CategoryEntity(
-            id = 999, //TODO: Id wird nicht automatisch vergeben
-            name = "Backen"
-        ),
-        creator = UserEntity(
-            id = 1, //TODO: Id wird nicht automatisch vergeben
-            firstName = "Root",
-            lastName = "Administrator",
-            email = "root@admin.com",
-            password = "geheim"
-        ),
+        categoryId = 2,
+        creatorId = 2,
+        createTime = LocalDate.now(),
+        difficultyId = 2,
+        image = "null",
+        longDescription = "null",
+        ingredients = listOf(IngredientRecipe.WithoutRecipe(
+            ingredientName = "Milch",
+            unit = "liter",
+            amount = 400))
     )
 
-    /*
-    @Before
-    fun clearEntries() {
-        // Clear all entries
-    }
-     */
-
-    @Test
-    fun testPostRecipe() {
-        // Test correct POST
+    @BeforeEach
+    @Description("Add testRecipe to database")
+    fun postRecipe(){
         val apiResponse = controller.createNewRecipe(testRecipe.toDto())
-        assertEquals(true, apiResponse.success)
+        id = apiResponse.data!!.id
+    }
 
-        if (apiResponse.success) {
-            id = apiResponse.data!!.id
+    @Test
+    @Description("Test correct RecipeController.findRecipeById(id) with existing id")
+    fun testCorrectGetRecipe(){
+        val apiResponse = controller.findRecipeById(id)
+        assertEquals(true, apiResponse.success)
+    }
+
+    @Test
+    @Description("Test faulty RecipeController.findRecipeById(id) with non-existing id")
+    fun testFaultyGetRecipe() {
+        val apiResponse = controller.findRecipeById(id + 1)
+        assertEquals(false, apiResponse.success)
+    }
+    @Test
+    @Description("")
+    fun testCorrectPutRecipe() {
+        val testRecipe2 = testRecipe
+        testRecipe2.title = "new title"
+
+        val apiResponse = controller.updateRecipe(id, testRecipe2.toDto())
+        assertEquals(true, apiResponse.success)
+        assertEquals("new title", apiResponse.data?.title ?: "")
+    }
+
+    @Test
+    @Description("")
+    fun testFaultyPutRecipe() {
+        val apiResponse = controller.updateRecipe(id + 1, testRecipe.toDto())
+        assertEquals(false, apiResponse.success)
+    }
+
+    @Test
+    @Description("Test correct RecipeController.deleteRecipe(id) with existing id")
+    fun testCorrectDeleteRecipe() {
+        controller.deleteRecipe(id)
+        val apiResponse = controller.findRecipeById(id)
+        assertEquals(false, apiResponse.success)
+    }
+
+    @Test
+    @Description("Test faulty RecipeController.deleteRecipe(id) with non-existing id")
+    fun testFaultyDeleteRecipe() {
+        org.junit.jupiter.api.assertThrows<ResponseStatusException> {
+            controller.deleteRecipe(id + 1)
         }
-
-        println("Recipe saved to id: $id")
     }
-
-    @Test
-    fun testGetRecipe() {
-        // Test correct GET
-        var apiResponse = controller.findRecipeById(id)
-        assertEquals(true, apiResponse.success)
-
-        // Test faulty GET
-        apiResponse = controller.findRecipeById(1234)
-        assertEquals(false, apiResponse.success)
-    }
-
-    /*
-    @Test
-    fun testPutRecipe() {
-        // change id
-        // test GET
-    }
-
-     */
-
-    /*
-    @Test
-    fun testDeleteRecipe() {
-        // Test correct DELETE
-        var apiResponse = controller.findRecipeById(3)
-        assertEquals(true, apiResponse.success)
-
-        controller.deleteRecipe(0)
-        apiResponse = controller.findRecipeById(3)
-        assertEquals(false, apiResponse.success)
-
-        // Test faulty DELETE
-        controller.deleteRecipe(10)
-
-        // TODO: Delete-ApiResponse has no success-field
-
-    }
-
-     */
 }
