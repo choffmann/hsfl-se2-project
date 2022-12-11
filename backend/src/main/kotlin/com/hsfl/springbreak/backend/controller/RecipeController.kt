9aebdb65ecfbfb809a0552c7063a99b6bf64b3d6
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2022-12-11 19:01:17 +0100

    refactor recipeControlller

--- backend/src/main/kotlin/com/hsfl/springbreak/backend/controller/RecipeController.kt
+++ backend/src/main/kotlin/com/hsfl/springbreak/backend/controller/RecipeController.kt
@@ -2,16 +2,8 @@ package com.hsfl.springbreak.backend.controller
 
 import com.hsfl.springbreak.backend.model.ApiResponse
 import com.hsfl.springbreak.backend.model.Recipe
-import com.hsfl.springbreak.backend.repository.RecipeRepository
 import com.hsfl.springbreak.backend.service.RecipeJpaService
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.transaction.Transactional
 
 /**
@@ -20,144 +12,33 @@ import javax.transaction.Transactional
 @CrossOrigin("http://localhost:3000")
 @Transactional
 @RestController
-class RecipeController(val recipeRepository: RecipeRepository, val recipeService: RecipeJpaService) {
+class RecipeController(val recipeService: RecipeJpaService) {
 
-   @GetMapping("recipes/{id}")
-   fun getRecipeById(@PathVariable("id") recipeId: Long): ApiResponse<Recipe> =
-      recipeService.getRecipeById(recipeId)
+    @GetMapping("recipes/{id}")
+    fun getRecipeById(@PathVariable("id") id: Long): ApiResponse<Recipe> =
+        recipeService.getRecipeById(id)
 
-   @GetMapping("recipes/findByName/{name}")
-   fun getRecipeByName(@PathVariable("name") recipeName: String): ApiResponse<Recipe> =
-      recipeService.getRecipeByName(recipeName)
+    @GetMapping("recipes/findByName/{name}")
+    fun getRecipeByName(@PathVariable("name") name: String): ApiResponse<Recipe> =
+        recipeService.getRecipeByName(name)
 
-   @PostMapping("recipes")
-   fun createNewRecipe(@RequestBody newRecipe: Recipe.CreateRecipe): ApiResponse<Recipe> =
-      recipeService.createRecipe(newRecipe)
+    @PostMapping("recipes")
+    fun createRecipe(@RequestBody recipe: Recipe.CreateRecipe): ApiResponse<Recipe> =
+        recipeService.createRecipe(recipe)
 
-   @PutMapping("recipes")
-   fun updateRecipe(@RequestBody changes: Recipe.ChangeRecipe) =
-      recipeService.updateRecipe(changes)
+    @PutMapping("recipes")
+    fun updateRecipe(@RequestBody changes: Recipe.ChangeRecipe) =
+        recipeService.updateRecipe(changes)
 
-   @DeleteMapping("recipes/{id}")
-   fun deleteRecipe(@PathVariable("id") recipeId: Long) =
-      recipeService.deleteRecipeById(recipeId)
+    @DeleteMapping("recipes/{id}")
+    fun deleteRecipe(@PathVariable("id") id: Long) =
+        recipeService.deleteRecipeById(id)
 
-   @PostMapping("recipe/favorite/{rId}/{uId}")
-   fun setFavoriteById(@PathVariable("rId") rId: Long, @PathVariable("uId") uId: Long) =
-      recipeService.addFavoriteById(rId, uId)
+    @PostMapping("recipe/favorite/{rId}/{uId}")
+    fun setFavoriteById(@PathVariable("rId") rId: Long, @PathVariable("uId") uId: Long) =
+        recipeService.addFavoriteById(rId, uId)
 
-   @GetMapping("recipe/favorite/{id}")
-   fun getFavoritesById(@PathVariable("id") id: Long): ApiResponse<List<Recipe>> =
-      recipeService.getFavoritesById(id)
-
-
-   /*
-   @GetMapping("api/recipes/{id}")
-   fun findRecipeById(@PathVariable("id") recipeId: Long): ApiResponse<Recipe> {
-      recipeRepository.findByIdOrNull(recipeId)?.let { recipe ->
-         return ApiResponse(data = recipe.toDto(), success = true)
-      } ?: return ApiResponse(error = "No Recipe with the ID: $recipeId", success = false)
-   }
-
-
-   @PostMapping("api/recipes")
-   fun createNewRecipe(@RequestBody newRecipe: Recipe): ApiResponse<Recipe> {
-      val savedRecipe = recipeRepository.save(RecipeEntity.fromDto(newRecipe)).toDto()
-      print("-------- "+savedRecipe+"----- \n")
-      return if (savedRecipe != null) {
-         ApiResponse(data = savedRecipe, success = true)
-      } else {
-         ApiResponse(error = "Error by saving", success = false)
-      }
-   }
-
-
-   @PutMapping("api/recipes/{id}")
-   fun updateRecipe(@PathVariable("id") recipeId: Long, @RequestBody recipe: Recipe): ApiResponse<Recipe> {
-      return if (recipeRepository.existsById(recipeId)) {
-         val result =  recipeRepository.findById(recipeId).get()
-         result.title = RecipeEntity.fromDto(recipe).title
-         result.shortDescription = RecipeEntity.fromDto(recipe).shortDescription
-         result.price = RecipeEntity.fromDto(recipe).price
-         result.duration = RecipeEntity.fromDto(recipe).duration
-         result.difficulty = RecipeEntity.fromDto(recipe).difficulty
-         result.category = RecipeEntity.fromDto(recipe).category
-         result.creator = RecipeEntity.fromDto(recipe).creator
-         return ApiResponse(data = recipeRepository.save(result).toDto(), success = true)
-         /*
-        it.rating = RecipeEntity.fromDto(recipe).rating
-        it.difficulty = RecipeEntity.fromDto(recipe).difficulty
-        it.createTime = RecipeEntity.fromDto(recipe).createTime
-        it.ingredients = RecipeEntity.fromDto(recipe).ingredients
-        it.image = RecipeEntity.fromDto(recipe).image
-        it.longDescription = RecipeEntity.fromDto(recipe).longDescription
-        it.views = RecipeEntity.fromDto(recipe).views
-        it.userFavorites = RecipeEntity.fromDto(recipe).userFavorites
-        return@map recipeRepository.save(it)
-         */
-      } else return ApiResponse(error = "No Recipe with the ID: $recipeId", success = false)
-   }
-
-   @DeleteMapping("api/recipes/{id}")
-   fun deleteRecipe(@PathVariable("id")recipeId: Long): Unit =
-      if (recipeRepository.existsById(recipeId)) {
-         recipeRepository.deleteById(recipeId)
-      } else {
-         throw ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe with ID \"$recipeId\" doesn't exist")
-      }
-
-    */
-
-
-/*
-recipeRepository.findById(recipeId).map {
-      it.title = RecipeEntity.fromDto(recipe).title
-      it.shortDescription = RecipeEntity.fromDto(recipe).shortDescription
-      it.price = RecipeEntity.fromDto(recipe).price
-      it.duration = RecipeEntity.fromDto(recipe).duration
-      it.category = RecipeEntity.fromDto(recipe).category
-      it.creator = RecipeEntity.fromDto(recipe).creator
-      /*
-      it.rating = RecipeEntity.fromDto(recipe).rating
-      it.difficulty = RecipeEntity.fromDto(recipe).difficulty
-      it.createTime = RecipeEntity.fromDto(recipe).createTime
-      it.ingredients = RecipeEntity.fromDto(recipe).ingredients
-      it.image = RecipeEntity.fromDto(recipe).image
-      it.longDescription = RecipeEntity.fromDto(recipe).longDescription
-      it.views = RecipeEntity.fromDto(recipe).views
-      it.userFavorites = RecipeEntity.fromDto(recipe).userFavorites
-      return@map recipeRepository.save(it)
-       */
-   }.orElseGet {
-      // recipe.id = recipeId
-      return@orElseGet recipeRepository.save(RecipeEntity.fromDto(recipe)))
-   }.toDto()
- */
-
-   /*
-   @GetMapping("api/recipes/{id}")
-   fun getRecipeById(@PathVariable("id") recipeId: Long): ApiResponse<Recipe> {
-      recipeService.getRecipeById(recipeId)?.let { recipe ->
-         return ApiResponse(data = recipe.toDto(), success = true)
-      } ?: return ApiResponse(error = "No Recipe with the ID: $recipeId", success = false)
-   }
-
-   @PostMapping("api/recipes")
-   fun createRecipe(@RequestBody payload: Recipe): ApiResponse<Recipe>? =
-      ApiResponse(data = recipeService.createNewRecipe(payload)?.toDto(), success = true)
-      /*
-      recipeService.createNewRecipe(payload)?.let { recipe ->
-         return ApiResponse(data = recipe.toDto(), success = true)
-      } ?: return ApiResponse(error = "Create Error", success = false)
-      */
-
-   @PutMapping("api/recipes/{id}")
-   fun updateRecipe(@PathVariable("id") recipeId: Long, @RequestBody payload: Recipe): ApiResponse<Recipe>? =
-      ApiResponse(data = recipeService.updateRecipe(recipeId, payload)?.toDto(), success = true)
-
-   @DeleteMapping("api/recipes/{id}")
-   fun deleteRecipe(@PathVariable("id") recipeId: Long): Unit =
-      recipeService.deleteRecipeById(recipeId)
-
-    */
+    @GetMapping("recipe/favorite/{id}")
+    fun getFavoritesById(@PathVariable("id") id: Long): ApiResponse<List<Recipe>> =
+        recipeService.getFavoritesById(id)
 }
