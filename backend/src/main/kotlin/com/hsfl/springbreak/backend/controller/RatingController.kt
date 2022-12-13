1a04863016523ccd8b649e21e5734a53abe12af3
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2022-12-13 15:27:27 +0100

    Changed all routes to queryparameters

--- backend/src/main/kotlin/com/hsfl/springbreak/backend/controller/RatingController.kt
+++ backend/src/main/kotlin/com/hsfl/springbreak/backend/controller/RatingController.kt
@@ -1,45 +1,16 @@
 package com.hsfl.springbreak.backend.controller
 
-import com.hsfl.springbreak.backend.entity.RatingEntity
 import com.hsfl.springbreak.backend.model.ApiResponse
 import com.hsfl.springbreak.backend.model.Rating
-import com.hsfl.springbreak.backend.repository.RatingRepository
+import com.hsfl.springbreak.backend.service.RatingService
 import org.springframework.web.bind.annotation.*
 
 @CrossOrigin("http://localhost:3000")
 @RestController
-class RatingController(val ratingRepository: RatingRepository) {
+class RatingController(val ratingService: RatingService) {
 
     @PostMapping("api/rating")
-    fun createNewRating(@RequestBody rating: Rating): ApiResponse<Rating> {
-        val savedRating = ratingRepository.save(RatingEntity.fromDto(rating)).toDto()
-        print("-------- " + savedRating + "----- \n")
-        return if (savedRating != null) {
-            ApiResponse(data = savedRating, success = true)
-        } else {
-            ApiResponse(error = "Error by saving", success = false)
-        }
-    }
-
-    @PutMapping("api/rating/like/{id}")
-    fun addLike(@PathVariable("id") id: Long): ApiResponse<Rating> {
-        if (ratingRepository.existsById(id)) {
-            val result = ratingRepository.findById(id).get()
-            result.likes = result.likes + 1
-            return ApiResponse(data = ratingRepository.save(result).toDto(), success = true)
-
-        } else
-            return ApiResponse(error = "No Recipe with the ID: ${id} ", success = false)
-    }
-
-    @PutMapping("api/rating/dislike/{id}")
-    fun addDisLike(@PathVariable("id") id: Long): ApiResponse<Rating> {
-        return if (ratingRepository.existsById(id)) {
-            val result = ratingRepository.findById(id).get()
-            result.dislike = result.dislike + 1
-            return ApiResponse(data = ratingRepository.save(result).toDto(), success = true)
-
-        } else return ApiResponse(error = "No Recipe with the ID: ${id} ", success = false)
-    }
+    fun setRating(@RequestBody rating: Rating.SendRating): ApiResponse<Rating> =
+        ratingService.setRating(rating)
 
 }
