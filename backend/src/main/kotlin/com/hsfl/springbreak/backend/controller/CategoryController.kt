8857d0b9a3e9690820a365201b8fd396c94b56be
dido3296 <dimitri.dorn2@stud.hs-flensburg.de>
2022-12-14 21:32:13 +0100

    Dokumentation für Controller geschrieben

--- backend/src/main/kotlin/com/hsfl/springbreak/backend/controller/CategoryController.kt
+++ backend/src/main/kotlin/com/hsfl/springbreak/backend/controller/CategoryController.kt
@@ -7,15 +7,26 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * Call category-related functions from category-repository.
+ */
 @CrossOrigin("http://localhost:3000")
 @RestController
 class CategoryController(val categoryRepository: CategoryRepository) {
 
     /**
-     * Returns a list of all categories.
+     * API-Endpoint for getting a list of all available categories.
+     * @return API-Response with a list of all stored categories or an error
      */
     @GetMapping("api/categories")
     fun retrieveDifficulties(): ApiResponse<List<Category>> {
-        return ApiResponse(data = categoryRepository.findAll().map { it.toDto() }, success = true)
+        val categoryEntities = categoryRepository.findAll().distinct()
+        return if (categoryEntities.isNotEmpty()) {
+            val categories = categoryEntities.map { it.toDto() }
+            ApiResponse(data = categories, success = true)
+        } else {
+            ApiResponse(error = "No categories available", success = false)
+        }
+
     }
 }
